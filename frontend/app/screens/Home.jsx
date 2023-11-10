405d471412b08ab7a0846cadac0d4f9230779a3f
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-10 11:37:40 +0900

    creating home header

--- frontend/app/screens/Home.jsx
+++ frontend/app/screens/Home.jsx
@@ -4,20 +4,15 @@ import { COLORS, SIZES } from "../constants/theme";
 import { SafeAreaView } from "react-native-safe-area-context";
 import pages from './page.style'
 import uidata from "../constants/uidata";
-import { UserReversedGeoCode } from "../context/UserReversedGeoCode";
-import { UserLocationContext } from "../context/UserLocationContext";
+import HomeHeader from "../components/HomeHeader";
 
 const Home = () => {
-  const { address, setAddress } = useContext(UserReversedGeoCode);
-  const { location, setLocation } = useContext(UserLocationContext);
-  console.log(location);
 
-  // console.log(address);
   return (
     <SafeAreaView>
       <View style={pages.viewOne}>
         <View style={pages.viewTwo}>
-
+          <HomeHeader />
         </View>
       </View>
     </SafeAreaView>
