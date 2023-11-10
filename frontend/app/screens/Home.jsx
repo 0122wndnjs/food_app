8bb8b896e6ba9550d13c1f4d23b36d7f04444659
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-10 11:14:48 +0900

    getting user location with expo

--- frontend/app/screens/Home.jsx
+++ frontend/app/screens/Home.jsx
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useContext } from "react";
 import { COLORS, SIZES } from "../constants/theme";
 import { SafeAreaView } from "react-native-safe-area-context";
 import pages from './page.style'
 import uidata from "../constants/uidata";
+import { UserReversedGeoCode } from "../context/UserReversedGeoCode";
+import { UserLocationContext } from "../context/UserLocationContext";
 
 const Home = () => {
-  
+  const { address, setAddress } = useContext(UserReversedGeoCode);
+  const { location, setLocation } = useContext(UserLocationContext);
+  console.log(location);
+
+  // console.log(address);
   return (
     <SafeAreaView>
       <View style={pages.viewOne}>
