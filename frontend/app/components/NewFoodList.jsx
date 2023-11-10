310042ac5d1c32dc3dbff90107da03b1e7d0d4b6
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2023-11-10 16:52:53 +0900

    Working on FoodStack Navigator but issue occurs

--- frontend/app/components/NewFoodList.jsx
+++ frontend/app/components/NewFoodList.jsx
@@ -2,11 +2,15 @@ import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import uidata from "../constants/uidata";
 import FoodComponent from "./FoodComponent";
+import { useNavigation } from "@react-navigation/native";
 
 const NewFoodList = () => {
+  const navigation = useNavigation();
+
   const renderItem = ({ item }) => (
-    <FoodComponent item={item} onPress={() => {}} />
+    <FoodComponent item={item} onPress={() => navigation.navigate('food-nav')} />
   );
+
   return (
     <View style={{ marginLeft: 12, marginBottom: 10 }}>
       <FlatList
---
+++ frontend/app/screens/FoodPage.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const FoodPage = () => {
+  return (
+    <View>
+      <Text>FoodPage</Text>
+    </View>
+  );
+};
+
+export default FoodPage;
+
+const styles = StyleSheet.create({});
